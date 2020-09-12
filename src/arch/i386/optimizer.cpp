0072039b93fcbe847562a3282813e50316b9da82
Robbe De Greef <robbedegreef@gmail.com>
2020-09-12 21:58:32 +0200

    Removed unnecessary debug line

--- src/arch/i386/optimizer.cpp
+++ src/arch/i386/optimizer.cpp
@@ -36,7 +36,6 @@ bool OptimizerX86::graphCheck(RegisterGraph &graph)
                         int last = graph.statements().regList().addRegister(graph.statements().regList().lastReg() + 1);
                         graph.statements().insert(line, new OpQuad(OpQuad::Types::SPILLLOAD, tosearch, -1, last, quad->type()));
                         graph.statements().regList()[last].setLastOcc(line + 1);
-                        dbg_print("reg: " << graph.statements().regList()[last].virt() << " first: " << graph.statements().regList()[last].firstOcc() << " last: " << graph.statements().regList()[last].lastOcc());
                         quad->setArg2(last);
                         line++;
                     }
@@ -61,45 +60,5 @@ bool OptimizerX86::graphCheck(RegisterGraph &graph)
 
 void OptimizerX86::prepAssignRegisters(OpList &list)
 {
-    /// Ok so I had a small realization in the shower (yes I think abt programming in the shower)
-    /// why would I want to do this for x86, i know we need to load in registers
-    /// because, well, otherwise we trash the first register. BUT we can just in the
-    /// generator load in the first reg to the ret and preform the operation on that
-    /// register. Damn sometimes I overthink shit (i mean the code below)
-#if 0
-    RegisterList &regList = list.regList();
-    int lastReg = regList.lastReg() + 1;
-
-    OpList tmp = list;
-    int i = 0;
-    for (OpQuad *quad : tmp)
-    {   
-        switch (quad->operation())
-        {
-        case OpQuad::Types::INTLIT:
-            break;
-        
-        default:
-            /// @todo: refactor this because wow that's some amazing code reuse
-            if (quad->arg1() != -1)
-            {
-                int r = regList.addRegister(lastReg);
-                lastReg++;
-                list.insert(list.begin() + i, new OpQuad(OpQuad::Types::REG, quad->arg1(), -1, r, quad->type()));
-                i++;
-                quad->setArg1(r);
-            }
-
-            if (quad->arg2() != -1)
-            {
-                int r = regList.addRegister(lastReg);
-                lastReg++;
-                list.insert(list.begin() + i, new OpQuad(OpQuad::Types::REG, quad->arg2(), -1, r, quad->type()));
-                i++;
-                quad->setArg2(r);
-            }
-        }
-        i++;
-    }
-#endif
+    /// We don't need to do anything here for X86 (yet)
 }
