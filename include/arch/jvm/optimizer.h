19c3c7a893cf237b7f8b4b71fb0a9a43cef5f369
Robbe De Greef <robbedegreef@gmail.com>
2020-09-14 23:26:54 +0200

    Started on building a JVM generator

---
+++ include/arch/jvm/optimizer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <optimizer/optimizer.h>
+#include <optimizer/reggraph.h>
+
+class OptimizerJVM : public Optimizer
+{
+protected:
+    // bool graphCheck(RegisterGraph &graph);
+    // void prepAssignRegisters(OpList &list);
+
+};
